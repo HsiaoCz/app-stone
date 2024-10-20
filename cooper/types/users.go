a2d7064f4174d38e4ccb14d90f866abe580674f3
HsiaoCz <984274788@qq.com>
2024-10-20 21:43:44 +0800

    add some

--- cooper/types/users.go
+++ cooper/types/users.go
@@ -5,3 +5,9 @@ import "gorm.io/gorm"
 type Users struct {
 	gorm.Model
 }
+
+type UserInfo struct {
+	UserID string
+	Email  string
+	Role   bool
+}
--- ebuy/types/users.go
+++ ebuy/types/users.go
@@ -1,4 +1,9 @@
 package types
 
-
 type Users struct{}
+
+type UserInfo struct {
+	UserID string
+	Email  string
+	Role   bool
+}
