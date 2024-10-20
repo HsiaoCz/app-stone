a020c8f09286bc48d62af6eb76f5ecf14948ed5e
HsiaoCz <984274788@qq.com>
2024-10-20 21:07:49 +0800

    add some

--- pan-music/db/migrations/20241012020214_create_users_sql.sql
+++ pan-music/db/migrations/20241012020214_create_users_sql.sql
@@ -5,6 +5,7 @@ CREATE TABLE IF NOT EXISTS users(
     username text not null,
     email text unique not null,
     password_hash text not null,
+    role boolean not null,
     created_at datetime not null,
     updated_at datetime not null,
     deleted_at datetime
--- pan-music/types/users.go
+++ pan-music/types/users.go
@@ -7,5 +7,12 @@ type Users struct {
 	UserID       string `gorm:"column:user_id;" json:"user_id"`
 	Username     string `gorm:"column:username;" json:"username"`
 	Email        string `gorm:"column:email;" json:"email"`
-	PasswordHash string `gorm:"column:password_hash;" json:"password_hash"`
+	PasswordHash string `gorm:"column:password_hash;" json:"-"`
+	Role         bool   `gorm:"column:role;" json:"role"`
+}
+
+type UserInfo struct {
+	UserID string
+	Email  string
+	Role   bool
 }
