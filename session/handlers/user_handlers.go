d34ec6eee2dd0ce06985302bd1f659c075dfaabb
HsiaoCz <984274788@qq.com>
2024-12-03 09:10:09 +0800

    add some

--- session/handlers/user_handlers.go
+++ session/handlers/user_handlers.go
@@ -1,8 +1,13 @@
 package handlers
 
 import (
+	"net/http"
+	"time"
+
 	"github.com/HsiaoCz/app-stone/session/storage"
+	"github.com/HsiaoCz/app-stone/session/types"
 	"github.com/gofiber/fiber/v2"
+	"github.com/google/uuid"
 )
 
 type UserHandlers struct {
@@ -10,13 +15,48 @@ type UserHandlers struct {
 	session storage.SessionStoreInter
 }
 
-func UserHandlersInit(user storage.UserStoreInter,session storage.SessionStoreInter) *UserHandlers {
+func UserHandlersInit(user storage.UserStoreInter, session storage.SessionStoreInter) *UserHandlers {
 	return &UserHandlers{
-		user: user,
+		user:    user,
 		session: session,
 	}
 }
 
 func (u *UserHandlers) HandleCreateUser(c *fiber.Ctx) error {
-	return nil
+	var user_create_params types.CreateUserParams
+	if err := c.BodyParser(&user_create_params); err != nil {
+		return ErrorMessage(http.StatusBadRequest, err.Error())
+	}
+	msg := user_create_params.Validate()
+	if len(msg) != 0 {
+		return c.Status(http.StatusBadRequest).JSON(msg)
+	}
+	user := types.NewUserFromParams(user_create_params)
+	userr, err := u.user.CreateUser(c.Context(), user)
+	if err != nil {
+		return ErrorMessage(http.StatusInternalServerError, err.Error())
+	}
+	sessionu := &types.Sessions{
+		Token:     uuid.New().String(),
+		UserID:    userr.ID.String(),
+		IpAddress: c.IP(),
+		UserAgent: string(c.Context().UserAgent()),
+		ExpiresAt: time.Now().Add(time.Hour * 24 * 30),
+	}
+	sessionr, err := u.session.CreateSession(c.Context(), sessionu)
+	if err != nil {
+		return ErrorMessage(http.StatusInternalServerError, err.Error())
+	}
+	c.Cookie(&fiber.Cookie{
+		Name:     "token",
+		Value:    sessionr.Token,
+		HTTPOnly: true,
+		Expires:  time.Now().Add(time.Hour * 24 * 30),
+	})
+	return c.Status(http.StatusOK).JSON(fiber.Map{
+		"status":  http.StatusOK,
+		"message": "create user success",
+		"user":    userr,
+		"session": sessionr,
+	})
 }
