4997cfc22d40e5bc799fe6e0dde5ff26fccf0333
HsiaoCz <984274788@qq.com>
2024-12-08 07:44:46 +0800

    add some

--- ebuy/storage/user_store.go
+++ ebuy/storage/user_store.go
@@ -4,9 +4,27 @@ import (
 	"context"
 
 	"github.com/HsiaoCz/app-stone/ebuy/types"
+	"gorm.io/gorm"
 )
 
+type UserStorer interface {
+	CreateUser(context.Context, *types.Users) (*types.Users, error)
+}
 
-type UserStorer interface{
-	CreateUser(context.Context,*types.Users)(*types.Users,error)
+type UserStore struct {
+	db *gorm.DB
+}
+
+func UserStoreInit(db *gorm.DB) *UserStore {
+	return &UserStore{
+		db: db,
+	}
+}
+
+func (u *UserStore) CreateUser(ctx context.Context, user *types.Users) (*types.Users, error) {
+	tx := u.db.Debug().WithContext(ctx).Model(&types.Users{}).Create(user)
+	if tx.Error != nil {
+		return nil, tx.Error
+	}
+	return user, nil
 }
